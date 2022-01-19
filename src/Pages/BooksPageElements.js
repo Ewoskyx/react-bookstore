f39adc1bd1df15c509d2ce100a30a8ee8b747dc9
Ewoskyx <ewoskyx@gmail.com>
2022-01-19 21:02:54 +0300

    Add redux add-remove book functionalities

---
+++ src/Pages/BooksPageElements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PageSection = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: auto;
+gap: 12px;
+`;
+
+export default PageSection;
--- src/components/NewBook/NewBookElements.js
+++ src/components/NewBook/NewBookElements.js
@@ -1,31 +1,35 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 100vh;
-  height: 200px;
+  width: 60%;
+  height: 100px;
+  padding: 12px;
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin-left: 400px;
+  background: #d1d1d1;
+  box-shadow: -2px -2px 0 #ffffff70, 3px 3px 10px #00000070;
 `;
 export const Heading = styled.h2`
   opacity: 0.7;
 `;
-export const Container = styled.div`
+export const Container = styled.form`
   width: 100%;
-  height: 25%;
+  height: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 12px;
 `;
 export const NewInput = styled.input`
-  width: 40%;
-  height: 60%;
+  width: 35%;
+  height: 70%;
+  padding: 8px;
 `;
 export const CategorySelect = styled.select`
   width: 40%;
-  height: 60%;
+  height: 70%;
+  padding: 8px|
 `;
 export const AddButton = styled.button`
   width: 20%;
